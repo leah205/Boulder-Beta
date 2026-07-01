5df88064aaaacd67d3b745827e59463750be7a39
leah205 <leahtiktin@gmail.com>
2026-07-01 08:54:39 -0400

    create post signup tests

--- server/src/auth/tests/auth.test.ts
+++ server/src/auth/tests/auth.test.ts
@@ -1,12 +1,12 @@
 import request from "supertest";
 import express from "express";
 import auth_router from "../auth_routes";
-import { beforeEach, it, describe, test } from "vitest";
+import { beforeEach, it, describe } from "vitest";
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use("/v1/api/auth", auth_router);
+app.use("/", auth_router);
 import prisma from "@/db/prisma_client";
 
 beforeEach(async () => {
@@ -16,13 +16,53 @@ beforeEach(async () => {
   ]);
 });
 
-test("post signup route works", (done) => {
-  request(app)
-    .post("/signup")
-    .set("Content-Type", "application/json")
-    .send({ username: "leah", password: "tiktin" })
-    .expect(function (res) {
-      res.body.username = "dave";
-    })
-    .expect(200, done);
+describe("POST /signup", () => {
+  it("returns username when user signs up", () => {
+    return request(app)
+      .post("/signup")
+      .type("form")
+      .send({
+        username: "leah",
+        password: "tiktin",
+        password_confirm: "tiktin",
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body.username).toEqual("leah");
+      });
+  });
+
+  it("returns errors when fields are empty", () => {
+    return request(app)
+      .post("/signup")
+      .type("form")
+      .send({
+        username: "leah",
+        password: "",
+        password_confirm: "",
+      })
+      .expect(400)
+      .then((res) => {
+        expect(res.body).toHaveProperty("errors");
+        expect(res.body.errors).toHaveLength(2);
+      });
+  });
+
+  it("returns errors when password does not match confirm password field", () => {
+    return request(app)
+      .post("/signup")
+      .type("form")
+      .send({
+        username: "leah",
+        password: "holla",
+        password_confirm: "hello",
+      })
+      .expect(400)
+      .then((res) => {
+        expect(res.body).toHaveProperty("errors");
+        expect(res.body.errors).toHaveLength(1);
+      });
+  });
 });
