054e18cd8237ebe88a8e0a68cc27ef1897f6e81e
leah205 <leahtiktin@gmail.com>
2026-07-04 16:05:31 -0400

    add styling to climb cards

--- client/src/pages/MyClimbsPage.tsx
+++ client/src/pages/MyClimbsPage.tsx
@@ -9,92 +9,20 @@ type climbCardProps = {
 
 function ClimbCard(props: climbCardProps) {
   const climb = props.climb;
+  const bgStyle = climb.picture
+    ? { backgroundImage: `url(${climb.picture})`, backgroundSize: "cover" }
+    : undefined;
   return (
-    <div className=" text-center hover:bg-mist-50 p-6 rounded-md border-1 border-mist-300 shadow-sm w-50 h-50">
-      {climb.picture && <img src={climb.picture}></img>}
-      {climb.grade && <p>Grade: {climb.grade}</p>}
-      {climb.rating && <p>Rating: {climb.rating}</p>}
-      {climb.sent && <p>sent!</p>}
+    <div
+      className={`text-center hover:bg-mist-50 p-6 rounded-md border-1 border-mist-300 shadow-sm w-50 h-50`}
+      style={bgStyle}
+    >
+      {climb.grade && (
+        <p className="text-white bg-black rounded-sm">{climb.grade}</p>
+      )}
     </div>
   );
 }
-
-// function EditModal(){
-//   const [formData, setFormData] = useState<Partial<Climb>>({});
-//     const { logClimb, isPending, errors } = useClimbLog();
-//     function handleSubmit() {
-//       logClimb(formData);
-//     }
-//   return (
-//         <Form>
-//           <ul>
-//             {errors &&
-//               errors.map((error) => {
-//                 return <ValidationError key={error}>{error}</ValidationError>;
-//               })}
-//           </ul>
-
-//           <FormField name="grade" label="Grade: ">
-//             <select
-//               name="grade"
-//               onChange={(e) =>
-//                 setFormData({ ...formData, grade: e.target.value })
-//               }
-//               value={formData.grade || "V0"}
-//             >
-//               {get_grades().map((grade) => {
-//                 return <option value={grade}>{grade}</option>;
-//               })}
-//             </select>
-//           </FormField>
-
-//           <FormField name="attempt_num" label="Attempts">
-//             <InputField
-//               value={formData.attempt_num}
-//               onChange={(e) =>
-//                 setFormData({ ...formData, attempt_num: Number(e.target.value) })
-//               }
-//               name="attempt_num"
-//               type="number"
-//             ></InputField>
-//           </FormField>
-
-//           <FormField name="sent" label="Sent">
-//             <input
-//               checked={formData.sent}
-//               type="checkbox"
-//               onChange={(e) => {
-//                 setFormData({
-//                   ...formData,
-//                   sent: e.target.checked,
-//                 });
-//               }}
-//               name="sent"
-//             ></input>
-//           </FormField>
-
-//           {formData.sent && (
-//             <FormField name="rating" label="Rating">
-//               <select
-//                 name="rating"
-//                 className="block"
-//                 onChange={(e) =>
-//                   setFormData({ ...formData, rating: Number(e.target.value[0]) })
-//                 }
-//                 value={`${formData.rating}/5` || "1/5"}
-//               >
-//                 {get_ratings().map((rating) => {
-//                   return <option value={rating}>{rating}</option>;
-//                 })}
-//               </select>
-//             </FormField>
-//           )}
-//           <Button type="submit" className="" onClick={handleSubmit}>
-//             Log Climb
-//           </Button>
-//         </Form>
-//   )
-// }
 
 export default function LogClimbPage() {
   const { isPending, error, data } = useQuery({
