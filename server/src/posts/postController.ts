43c81443ab32aa5881898e04a53510affd74156a
leah205 <leahtiktin@gmail.com>
2026-07-26 12:45:45 -0400

    refactor and fix: pagination

--- server/src/posts/postController.ts
+++ server/src/posts/postController.ts
@@ -4,19 +4,10 @@ import { FeedResponse, PostResponse } from "@shared/types";
 import { id } from "zod/v4/locales";
 
 const postController = {
-  // getPublicFeed: async (req: Request, res: Response) => {
-  //   const feed = (await postQueries.getAllPublished()) satisfies PostResponse[];
-  //   console.log(typeof feed[0].uploadedAt);
-  //   res.json(feed);
-  // },
-
   getFeedPage: async (req: Request, res: Response) => {
     const cursor = req.query.cursor?.length ? req.query.cursor : null;
-    const cursorType = req.query.cursorType;
-    console.log(cursorType);
     const nextFeedPage = (await postQueries.getFeedPage(
       cursor as string | null,
-      cursorType as string | null,
     )) satisfies FeedResponse;
     res.json(nextFeedPage);
   },
@@ -30,14 +21,6 @@ const postController = {
     )) satisfies FeedResponse;
     res.json(nextFeedPage);
   },
-
-  // getFollowingPosts: async (req: Request, res: Response) => {
-  //   const user = req.user;
-  //   const feed = (await postQueries.getFollowingPosts(
-  //     user!.id,
-  //   )) satisfies PostResponse[];
-  //   res.json(feed);
-  // },
 
   deletePost: async (req: Request, res: Response) => {
     const id = Number(req.params.id);
