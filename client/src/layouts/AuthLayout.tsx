9748ca187c0715df66e5c187ac80e3759bafa26d
leah205 <leahtiktin@gmail.com>
2026-08-04 08:52:40 -0400

    styling: auth pages

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -3,6 +3,12 @@ import { Outlet } from "react-router-dom";
 export default function AuthLayout() {
   return (
     <>
+      <h1 className="text-center text-5xl text-blue-400 py-10">
+        Welcome to Boulder Beta
+      </h1>
+      <p className="text-center text-3xl text-blue-300 pb-6">
+        A personal project to enable easy climbing tracking and sharing.
+      </p>
       <Outlet />
     </>
   );
