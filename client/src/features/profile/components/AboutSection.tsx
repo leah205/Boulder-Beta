c96afb054e6380b1281de25d472de8465909857b
Leah Tiktin <leahtiktin@gmail.com>
2026-08-23 21:45:17 +0200

    add follow button to profile page

--- client/src/features/profile/components/AboutSection.tsx
+++ client/src/features/profile/components/AboutSection.tsx
@@ -1,12 +1,18 @@
 import ProfilePic from "@/components/ProfilePic";
 import type { UserResponse } from "@shared/types"
-
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useFollowUser from "@/hooks/useFollowUser"
+import Button from "@/components/Button";
 type AboutSectionProps = {
     user: UserResponse;
     isSelf: boolean;
 }
 
 export default function AboutSection({user}: AboutSectionProps){
+     const { toggleFollowUser, isFollowing, isPending, error } = useFollowUser(user.id)
+    const currentUser = useCurrentUser();
+    const isSelf = currentUser.id == user.id;
+     
     return <>
         <div className = "flex justify-between px-8 py-5">
             <ProfilePic size = '100' username = {user.username}></ProfilePic>
@@ -14,6 +20,15 @@ export default function AboutSection({user}: AboutSectionProps){
                <p className="text-xl mb-5">{user.username}</p>
             <p>Followers: {user.followedBy.length}</p>
             <p>Following: {user.following.length}</p>
+            {!isSelf && (
+                          <Button
+                            type="submit"
+                            className="text-xs height-5 py-1"
+                            onClick={toggleFollowUser}
+                            variant={isFollowing ? "red" : "blue"}
+                          >
+                            {isFollowing ? "Unfollow" : "Follow"}
+                          </Button> )}
         </div>
         </div>
          
